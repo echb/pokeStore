c68a6996acd2a20e5a6e17fdaf67cbb44ea90a1c
echbili <ech.emmanuel.ch@gmail.com>
2022-05-14 18:44:05 -0500

    rotes and firebase auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,68 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import NotFound from '../views/NotFound.vue'
+import singlePokemon from '../views/SinglePokemon.vue'
+import { getCurrentUser } from '../store/auth'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    // name: 'inde',
+    redirect: '/home'
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/home',
+    name: 'home',
+    component: HomeView,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
+  },
+  {
+    path: '/pokemon/:id',
+    name: 'singlePokemon',
+    component: singlePokemon,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'notfound',
+    component: NotFound
+  },
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (await getCurrentUser()) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+});
 
 export default router
