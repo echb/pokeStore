61492a5155b2faf4c850473ce8e6b14a1bcbc65b
echbili <ech.emmanuel.ch@gmail.com>
2022-05-15 11:00:36 -0500

    v1

--- src/views/SinglePokemon.vue
+++ src/views/SinglePokemon.vue
@@ -1,8 +1,19 @@
 <template>
   <v-row>
     <v-col cols="12">
-      <v-card rounded="lg" min-height="268">
-        {{ id }}
+      <v-card rounded="lg" min-height="268" class="capital" :color="pokemon.types[0].type.name">
+        <v-img height="250" :src="pokemon.sprites.other.dream_world.front_default" aspect-ratio="2" contain></v-img>
+        <v-card-title> {{ pokemon.name }} </v-card-title>
+        <v-card-text :class="pokemon.types[0].type.name">
+          Height: {{ pokemon.height }}
+          Weight: {{ pokemon.weight }}
+          <v-chip-group column>
+            <v-chip :color="pokeType.type.name" v-for="pokeType in pokemon.types" :key="pokeType.type.name">{{
+                pokeType.type.name
+            }}
+            </v-chip>
+          </v-chip-group>
+        </v-card-text>
       </v-card>
     </v-col>
   </v-row>
@@ -10,24 +21,16 @@
 
 
 <script>
-
+import { getPokemonById } from "@/store/pokemonService"
 
 export default {
   data: () => ({
     id: null,
     pokemon: []
   }),
-
-  computed: {
-
-  },
-
-  methods: {
-
-  },
-  mounted() {
+  async created() {
+    this.pokemon = await getPokemonById(this.$route.params.id)
     this.id = this.$route.params.id
-    // console.log(this.$route.params.id);
   }
 }
 </script>
