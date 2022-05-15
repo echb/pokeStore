83430a6f5609278461a9c8f8d6cb0c6a4c9f9fce
echbili <ech.emmanuel.ch@gmail.com>
2022-05-15 10:00:49 -0500

    v1

--- src/store/pokemonsStore.js
+++ src/store/pokemonsStore.js
@@ -2,17 +2,26 @@ import Vue from 'vue';
 
 const pokeStore = Vue.observable({
   pokemons: [],
+  pokemonsChoosen: [],
 
   setPokemons(pokemons) {
     this.pokemons = pokemons
   },
-
   setLocalPokemons(pokemons) {
     localStorage.setItem('pokemonsData', JSON.stringify(pokemons))
   },
   getLocalPokemons() {
     return JSON.parse(localStorage.getItem('pokemonsData'))
   },
+  removeLocalChoosenPokemons() {
+    this.pokemonsChoosen = []
+  },
+  removeLocalPokemons() {
+    localStorage.removeItem('pokemonsData')
+  }
 });
 
-Vue.prototype.$storePokemon = pokeStore
+
+export {
+  pokeStore,
+}
