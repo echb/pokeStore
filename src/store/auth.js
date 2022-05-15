e0a1449d1c55dac77a6cb16b990e405e89346d7c
echbili <ech.emmanuel.ch@gmail.com>
2022-05-14 20:40:29 -0500

    auth firebase

---
+++ src/store/auth.js
@@ -0,0 +1,63 @@
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import Vue from 'vue';
+
+const authLogged = Vue.observable({
+  isLogged: false
+});
+
+Vue.prototype.$authLogged = authLogged
+
+const login = async (email, password) => {
+  try {
+    return await signInWithEmailAndPassword(getAuth(), email, password)
+  } catch (error) {
+    return false
+  }
+}
+
+const SignUp = async (email, password) => {
+  try {
+    return await createUserWithEmailAndPassword(getAuth(), email, password)
+  } catch (error) {
+    return false
+  }
+}
+
+const logOut = () => {
+  signOut(getAuth())
+}
+
+
+const authChanged = () => {
+  const auth = getAuth()
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      authLogged.isLogged = true
+    } else {
+      authLogged.isLogged = false
+    }
+  })
+
+}
+
+const getCurrentUser = () => {
+  return new Promise((res, rej) => {
+    const removes = onAuthStateChanged(
+      getAuth(), (user) => {
+        removes()
+        res(user)
+      },
+      rej
+    )
+  })
+}
+
+
+export {
+  SignUp,
+  login,
+  logOut,
+  authChanged,
+  getCurrentUser
+}
