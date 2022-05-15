1b1a653f9735d347f223f7e978df7bfc7692493b
echbili <ech.emmanuel.ch@gmail.com>
2022-05-14 20:36:36 -0500

    add cards components

---
+++ src/components/Cards.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-col cols="12" sm="9">
+    <v-row>
+      <v-col v-for="pokemon in pokemons" :key="pokemon.name" cols="6" sm="4">
+        <Card :pokemon="pokemon"></Card>
+      </v-col>
+    </v-row>
+  </v-col>
+</template>
+
+<script>
+import Card from './Card.vue'
+export default {
+  props: ['pokemons'],
+  components: {
+    'Card': Card
+  }
+}
+</script>
