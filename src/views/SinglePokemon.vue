6a1c354847a3b3c1650ef98624470fa92675107c
echbili <ech.emmanuel.ch@gmail.com>
2022-05-14 20:41:04 -0500

    single pokemon view

---
+++ src/views/SinglePokemon.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-card rounded="lg" min-height="268">
+        {{ id }}
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+
+<script>
+
+
+export default {
+  data: () => ({
+    id: null,
+    pokemon: []
+  }),
+
+  computed: {
+
+  },
+
+  methods: {
+
+  },
+  mounted() {
+    this.id = this.$route.params.id
+    // console.log(this.$route.params.id);
+  }
+}
+</script>
