02c5ba3bb2b2955a45f6f86553f1da12e4d784d7
echbili <ech.emmanuel.ch@gmail.com>
2022-05-15 10:04:18 -0500

    v1 components created

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,18 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-      app
-      color="white"
-      flat
-    >
-      <v-avatar
-        :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-        size="32"
-      ></v-avatar>
-
-      <v-tabs
-        centered
-        class="ml-n9"
-        color="grey darken-1"
-      >
-        <v-tab
-          v-for="link in links"
-          :key="link"
-        >
-          {{ link }}
-        </v-tab>
-      </v-tabs>
-
-      <v-avatar
-        class="hidden-sm-and-down"
-        color="grey darken-1 shrink"
-        size="32"
-      ></v-avatar>
-    </v-app-bar>
-
+    <Toolbar></Toolbar>
     <v-main class="grey lighten-3">
       <v-container>
-        <v-row>
-          <!-- <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-            </v-sheet>
-          </v-col> -->
-
-          <v-col
-            cols="12"
-            sm="8"
-          >
-            <v-sheet
-              min-height="70vh"
-              rounded="lg"
-            >
-              <!--  -->
-            </v-sheet>
-          </v-col>
-
-          <v-col
-            cols="12"
-            sm="2"
-          >
-            <v-sheet
-              rounded="lg"
-              min-height="268"
-            >
-            <p>asd</p>
-            <ul v-for="el in els" :key="el.id">
-              <li > {{el}} </li>
-            </ul>
-              <!--  -->
-            </v-sheet>
-          </v-col>
-        </v-row>
+        <router-view></router-view>
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      links: [
-        'Dashboard',
-        'Messages',
-        'Profile',
-        'Updates',
-      ],
-      els:Array.from(Array(100).keys())
-    }),
-  }
+import Toolbar from './components/Toolbar.vue'
+
+export default {
+  components: { 'Toolbar': Toolbar }
+}
 </script>
